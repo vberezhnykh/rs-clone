d93a3ad7342164a19b001e9437d9f2246990e569
Farkhod Kodirov <farxod94@gmail.com>
2023-02-01 23:04:33 +0500

    feat: implement templates (search, mixer, account)

--- src/pages/accountPage/accountPage.ts
+++ src/pages/accountPage/accountPage.ts
@@ -6,8 +6,18 @@ class AccountPage implements InterfaceContainerElement {
     const main = createHTMLElement('main', 'main');
 
     main.innerHTML = `
-    <div class="main__container">
-      Account Page
+    <div class="main__container container">
+      
+      <div class="account">
+        <div class="account__inner">
+          <h3>Вход</h3>
+          <input type="email" placeholder="Электронная почта">
+          <input type="password" placeholder="Пароль">
+          <button class="button"><span>Войти</span></button>
+          <button class="button button-1"><span>Зарегистрироваться</span></button>
+        </div>
+      </div>
+
     </div>
     `;
 
