7a7b373f6bfd6814e155a8a5756a3d31558c452b
Farkhod Kodirov <farxod94@gmail.com>
2023-02-07 00:00:17 +0500

    feat: add links to footer

--- src/components/footer/footer.ts
+++ src/components/footer/footer.ts
@@ -10,9 +10,9 @@ class Footer implements InterfaceContainerElement {
     footer.innerHTML = `
       <div class="footer__container container">
         <div class="footer__github">
-        <img src="${logogithub}" alt="Github" height="32"> flash226 | fkodirov | vberezhnykh</div>
+        <img src="${logogithub}" alt="Github" height="32"> <a href="https://github.com/Flash226/" target="_blank">flash226</a> | <a href="https://github.com/fkodirov/" target="_blank">fkodirov</a> | <a href="https://github.com/vberezhnykh/" target="_blank">vberezhnykh</a></div>
         <p class="footer__text">2023</p>
-        <div class="footer__rs"><img src="${logors}" alt="RS School" height="32"></div>
+        <div class="footer__rs"><a href="https://rs.school/js/" target="_blank"><img src="${logors}" alt="RS School" height="32"></a></div>
       </div>
       `;
 
