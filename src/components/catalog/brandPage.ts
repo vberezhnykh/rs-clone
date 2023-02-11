bd6ba5a53b5d5d46cd57feb67994fa80c3db281b
Valentin Berezhnykh <vberezhnykh74@gmail.com>
2023-02-11 21:01:03 +0300

    feat: add a search by flavor name

--- src/components/catalog/brandPage.ts
+++ src/components/catalog/brandPage.ts
@@ -1,18 +1,24 @@
 import { createHTMLElement } from '../../utils/createHTMLElement';
 import Api from '../api/api';
 import { Flavors, InterfaceContainerElement } from '../types/types';
+import backArrowImgSrc from '../../assets/images/back-arrow-white.png';
 import searchImgSrc from '../../assets/images/search.svg';
 import infoImgSrc from '../../assets/images/info.svg';
 import addNewImgSrc from '../../assets/images/add-new.png';
+import preloader from '../preloader/preloader';
+
+const ERROR_MESSAGE = 'К сожалению, по вашему запросу ничего не найдено...';
 
 export class BrandPage implements InterfaceContainerElement {
   private brand: string;
   private flavors: Flavors;
   api: Api;
+  preloader: preloader;
   constructor() {
     this.brand = window.location.hash.split('/')[3];
     this.brand = this.brand.replace('%20', ' ');
     this.api = new Api();
+    this.preloader = new preloader();
   }
   draw() {
     const brandPage = createHTMLElement('brand-page');
@@ -34,7 +40,8 @@ export class BrandPage implements InterfaceContainerElement {
   }
 
   private createHeaderBackBtn() {
-    const backBtn = createHTMLElement('brand__back-button', 'button', '←');
+    const backBtn = createHTMLElement('brand__back-button', 'button');
+    backBtn.style.backgroundImage = `url(${backArrowImgSrc})`;
     backBtn.onclick = () => history.back();
     return backBtn;
   }
@@ -58,22 +65,39 @@ export class BrandPage implements InterfaceContainerElement {
   private createSearchInput() {
     const searchInput = <HTMLInputElement>createHTMLElement('search__input', 'input');
     searchInput.placeholder = 'Поиск';
-    // searchInput.onkeyup = () => this.handleKeyupOnInput(searchInput);
+    searchInput.onkeyup = () => this.handleKeyupOnInput(searchInput);
     return searchInput;
   }
 
-  private async createFlavorsList() {
-    if (!this.flavors) await this.getAllFlavorsByBrand();
+  private async handleKeyupOnInput(input: HTMLInputElement) {
+    const sortedFlavors = this.flavors.filter((flavor) =>
+      flavor.name.toLowerCase().includes(input.value.toLowerCase())
+    );
+    const flavorsList = document.querySelector('.flavor-list');
+    if (!flavorsList) return;
+    flavorsList.replaceWith(await this.createFlavorsList(sortedFlavors));
+  }
+
+  private async createFlavorsList(flavors?: Flavors) {
     const flavorsList = createHTMLElement('flavor-list', 'ul');
-    for (let i = 0; i < this.flavors.length; i++) {
-      flavorsList.appendChild(this.createFlavorListItem(i));
+    if (flavors?.length === 0) return this.showErrorMessage(flavorsList);
+    if (!this.flavors) await this.getAllFlavorsByBrand();
+    const flavorsToIterate = flavors ?? this.flavors;
+    for (let i = 0; i < flavorsToIterate.length; i++) {
+      flavorsList.appendChild(this.createFlavorListItem(i, flavorsToIterate));
     }
     return flavorsList;
   }
 
-  private createFlavorListItem(i: number) {
+  private showErrorMessage(flavorsList: HTMLElement) {
+    flavorsList.append(ERROR_MESSAGE);
+    flavorsList.classList.add('catalog-list--error');
+    return flavorsList;
+  }
+
+  private createFlavorListItem(i: number, flavors: Flavors) {
     const flavorListItem = createHTMLElement('flavor-list__item', 'li');
-    flavorListItem.appendChild(createHTMLElement('flavor__name', 'span', this.flavors[i].name));
+    flavorListItem.appendChild(createHTMLElement('flavor__name', 'span', flavors[i].name));
     flavorListItem.appendChild(this.createImage('flavor__image--info', infoImgSrc));
     flavorListItem.appendChild(this.createImage('flavor__image--add-new', addNewImgSrc));
     return flavorListItem;
@@ -87,8 +111,10 @@ export class BrandPage implements InterfaceContainerElement {
   }
 
   private async getAllFlavorsByBrand() {
+    this.preloader.draw();
     this.flavors = (await this.api.getAllFlavors()).filter(
       (flavor) => flavor.brand.toLowerCase() === this.brand.toLowerCase()
     );
+    this.preloader.removePreloader();
   }
 }
