e50c847ea88ac2fe34ef87fb1983ce947fc3fcc2
Yaroslav Nepiuk <slavanepuk@gmail.com>
2023-02-17 06:59:20 +0100

    fix: fixed some errors

--- src/pages/favorite_tabacos_page/favorite_tabacos_page.ts
+++ src/pages/favorite_tabacos_page/favorite_tabacos_page.ts
@@ -6,66 +6,177 @@ import Api from '../../components/api/api';
 import preloader from '../../components/preloader/preloader';
 import ApiMix from '../../components/api_mix/api_mix';
 import ProfileUser from '../../components/profile_user/profile_user';
+import { Brands, Flavors } from '../../components/types/types';
+import searchImgSrc from '../../assets/images/search.svg';
+import mixerButtonImgSrc from '../../assets/images/blender.svg';
+import { getFlavorsInMixer } from '../../utils/getFlavorsInMixer';
+
+
+import { Flavor } from '../../components/types/types';
+import infoImgSrc from '../../assets/images/info.svg';
+import addNewImgSrc from '../../assets/images/add-new.png';
+import { createPopup, openFlavorPopup } from '../../components/popup/popup';
+import { changeFlavorNumInBrandPageHeader } from '../../utils/changeFlavorNum';
+
+const ERROR_MESSAGE = 'К сожалению, по вашему запросу ничего не найдено...';
+const MIXER_PAGE_URL = `/mixer`;
+const BRAND_SUGGEST_URL = '/brand-suggest';
 
 class FavoriteTobaccosPage implements InterfaceContainerElement {
-  mixes?: Mixes;
+  private brand: string;
+  private flavors: Flavors;
   api: Api;
+  apiMix;
   preloader: preloader;
-  private apiMix;
-  private profileUser;
   constructor() {
+    this.brand = 'Мои любимые табаки';
     this.api = new Api();
     this.preloader = new preloader();
     this.apiMix = new ApiMix();
-    this.profileUser = new ProfileUser();
   }
   draw() {
-    const main = createHTMLElement('main', 'main');
-    const container = createHTMLElement(['main__container', 'container']);
-    main.appendChild(container);
-    let UserMixesContainer;
-    this.createUserMixesPopup().then((data) => {
-      UserMixesContainer = data;
-      container.appendChild(UserMixesContainer);
-    });
-    return main;
-  }
+    // document.querySelector('.result-container')?.remove();
+    // const brandPage = createHTMLElement('brand-page');
+    // brandPage.appendChild(this.createHeader());
+    // brandPage.appendChild(this.createSearchPanel());
+    // this.createFlavorsList().then((element) => brandPage.appendChild(element));
+    // createPopup(brandPage);
+    this.api.getAllRate().then((data) => {
+      console.log(1111, data)
+    })
+    const header = createHTMLElement('brand-page-header', 'div');
 
-  private async createUserMixesPopup() {
-    const UserMixesContainer = createHTMLElement('user-mixes-container');
-    UserMixesContainer.appendChild(this.createUserMixesPopupHeader());
-    if (!this.mixes) {
-      this.preloader.draw();
-      const userId = this.profileUser.getUserId();
-      if (typeof userId === 'string') {
-        const favoriteMixId = await this.apiMix.getFavorite(userId);
-        const favoriteMix = [];
-        for (let i = 0; i < favoriteMixId.length; i += 1) {
-          favoriteMix.push(await this.api.getMix(favoriteMixId[i]));
-        }
-        this.mixes = favoriteMix;
-      }
-      this.preloader.removePreloader();
-    }
-    const userMixesList = new MixesList(this.mixes).create();
-    UserMixesContainer.appendChild(userMixesList);
-    return UserMixesContainer;
-  }
-
-  private createUserMixesPopupHeader() {
-    const header = createHTMLElement('user-mixes__header');
-    const navBar = createHTMLElement('user-mixes__nav', 'nav');
-    const backArrowImage = new Image();
-    backArrowImage.className = 'user-mixes__back-arrow';
-    backArrowImage.src = backArrowImgSrc;
-    backArrowImage.onclick = () => (location.hash = '/account');
-    navBar.append(backArrowImage);
-    header.append(navBar);
-    const heading = createHTMLElement('user-mixes__heading', 'h4');
-    heading.textContent = 'Мои табаки';
-    header.append(heading);
     return header;
   }
+
+  // private createHeader() {
+  //   const header = createHTMLElement('brand-page-header', 'div');
+  //   header.appendChild(this.createHeaderBackBtn());
+  //   header.appendChild(createHTMLElement('brand-page__title', 'h2', this.getBrandName()));
+  //   header.appendChild(this.createHeaderMixerButton());
+  //   return header;
+  // }
+
+  // private getBrandName() {
+  //   return this.brand[0].toUpperCase() + this.brand.slice(1);
+  // }
+
+  // private createHeaderBackBtn() {
+  //   const backBtn = createHTMLElement('brand__back-button', 'button');
+  //   backBtn.style.backgroundImage = `url(${backArrowImgSrc})`;
+  //   backBtn.onclick = () => history.back();
+  //   return backBtn;
+  // }
+
+  // private createHeaderMixerButton() {
+  //   const mixerBtn = createHTMLElement('catalog__mixer-image', 'button');
+  //   mixerBtn.style.backgroundImage = `url(${mixerButtonImgSrc})`;
+  //   const flavorsInMixerNum = getFlavorsInMixer().length;
+  //   if (flavorsInMixerNum !== 0) {
+  //     mixerBtn.append(createHTMLElement('catalog__mixer-number', 'div', flavorsInMixerNum.toString()));
+  //   }
+  //   mixerBtn.onclick = () => (location.hash = '/mixer/mixer-now');
+  //   return mixerBtn;
+  // }
+
+  // private createSearchPanel() {
+  //   const searchPanel = createHTMLElement(['brand-page__search', 'search'], 'div');
+  //   const innerSearchPanelContainer = createHTMLElement('search__inner', 'div');
+  //   innerSearchPanelContainer.appendChild(this.createSearchInputImage());
+  //   innerSearchPanelContainer.appendChild(this.createSearchInput());
+  //   searchPanel.appendChild(innerSearchPanelContainer);
+  //   return searchPanel;
+  // }
+
+  // private createSearchInputImage() {
+  //   const image = new Image();
+  //   image.src = searchImgSrc;
+  //   image.alt = 'search';
+  //   return image;
+  // }
+
+  // private createSearchInput() {
+  //   const searchInput = <HTMLInputElement>createHTMLElement('search__input', 'input');
+  //   searchInput.placeholder = 'Поиск';
+  //   searchInput.onkeyup = () => this.handleKeyupOnInput(searchInput);
+  //   return searchInput;
+  // }
+
+  // private async handleKeyupOnInput(input: HTMLInputElement) {
+  //   const sortedFlavors = this.flavors.filter((flavor) =>
+  //     flavor.name.toLowerCase().includes(input.value.toLowerCase())
+  //   );
+  //   const flavorsList = document.querySelector('.flavor-list');
+  //   if (!flavorsList) return;
+  //   flavorsList.replaceWith(await this.createFlavorsList(sortedFlavors));
+  // }
+
+  // private async createFlavorsList(flavors?: Flavors) {
+  //   const flavorsList = createHTMLElement('flavor-list', 'ul');
+  //   if (flavors?.length === 0) return this.showErrorMessage(flavorsList);
+  //   if (!this.flavors) await this.getAllFlavorsByBrand();
+  //   const flavorsToIterate = flavors ?? this.flavors;
+  //   for (let i = 0; i < flavorsToIterate.length; i++) {
+  //     flavorsList.appendChild(this.createFlavorListItem(i, flavorsToIterate));
+  //   }
+  //   return flavorsList;
+  // }
+
+  // private showErrorMessage(flavorsList: HTMLElement) {
+  //   flavorsList.append(ERROR_MESSAGE);
+  //   flavorsList.classList.add('catalog-list--error');
+  //   return flavorsList;
+  // }
+
+  // private createFlavorListItem(i: number, flavors: Flavors) {
+  //   const flavorListItem = createHTMLElement('flavor-list__item', 'li');
+  //   flavorListItem.appendChild(createHTMLElement('flavor__name', 'span', flavors[i].name));
+  //   flavorListItem.appendChild(this.createImage('flavor__image--info', infoImgSrc, flavors[i]));
+  //   flavorListItem.appendChild(this.createImage('flavor__image--add-new', addNewImgSrc, flavors[i]));
+  //   return flavorListItem;
+  // }
+
+  // private createImage(className: string, src: string, flavor?: Flavor) {
+  //   const image = new Image();
+  //   image.className = className;
+  //   image.src = src;
+  //   if (className === 'flavor__image--info') {
+  //     if (!flavor) return image;
+  //     image.onclick = () => {
+  //       const addButton = image.nextElementSibling ?? undefined;
+  //       openFlavorPopup(flavor, addButton);
+  //     };
+  //   } else if (className === 'flavor__image--add-new') {
+  //     if (!flavor) return image;
+  //     if (this.isFlavorInMixer(flavor)) image.classList.add('flavor__image--added');
+  //     image.onclick = () => this.handleClickOnAddButton(image, flavor);
+  //   }
+  //   return image;
+  // }
+
+  // private isFlavorInMixer(flavor: Flavor) {
+  //   const flavorsInMixer = getFlavorsInMixer();
+  //   const index = flavorsInMixer.findIndex((flavorInMixer) => flavorInMixer.id === flavor.id);
+  //   return index !== -1;
+  // }
+
+  // private handleClickOnAddButton(image: HTMLImageElement, flavor: Flavor) {
+  //   image.classList.toggle('flavor__image--added');
+  //   const flavorsInMixer = getFlavorsInMixer();
+  //   const indexOfFlavorInMixer = flavorsInMixer.findIndex((flavorInMixer) => flavorInMixer.id === flavor.id);
+  //   if (image.classList.contains('flavor__image--added') && indexOfFlavorInMixer === -1) flavorsInMixer.push(flavor);
+  //   else flavorsInMixer.splice(indexOfFlavorInMixer, 1);
+  //   localStorage.setItem('flavors', JSON.stringify(flavorsInMixer));
+  //   changeFlavorNumInBrandPageHeader();
+  // }
+
+  // private async getAllFlavorsByBrand() {
+  //   this.preloader.draw();
+  //   this.flavors = (await this.api.getAllFlavors()).filter(
+  //     (flavor) => flavor.brand.toLowerCase() === this.brand.toLowerCase()
+  //   );
+  //   this.preloader.removePreloader();
+  // }
 }
 
 export default FavoriteTobaccosPage;
