d55b51a92cbfd99bf8a6145fbd32930c98a8629e
Farkhod Kodirov <farxod94@gmail.com>
2023-02-27 21:00:38 +0500

    fix: auth user rating star

--- src/components/getRatingStar/getRatingStar.ts
+++ src/components/getRatingStar/getRatingStar.ts
@@ -1,11 +1,24 @@
 import ratingStarIconSrc from '../../assets/images/star-empty.svg';
 import ratingGoldStarIconSrc from '../../assets/images/star-gold.svg';
 import { Rates } from '../types/types';
+import ProfileUser from '../../components/profile_user/profile_user';
 
 export default function getRatingStar(id: number) {
-  const checkRatesinLS = localStorage.getItem('blenderStartProfile');
+  const profileUser = new ProfileUser();
+  const idProfile = profileUser.getUserId();
+  const checkRatesStartinLS = localStorage.getItem('blenderStartProfile');
+  const checkRatesinLS = localStorage.getItem('blenderProfile');
   if (checkRatesinLS) {
-    const getRates = (<Rates>JSON.parse(checkRatesinLS).rating).map((e) => e.id);
+    if (idProfile !== false && idProfile.length > 12) {
+      const getRates = (<Rates>JSON.parse(checkRatesinLS).rating).map((e) => e.id);
+      if (getRates.includes(id)) {
+        return ratingGoldStarIconSrc;
+      } else {
+        return ratingStarIconSrc;
+      }
+    }
+  } else if (checkRatesStartinLS) {
+    const getRates = (<Rates>JSON.parse(checkRatesStartinLS).rating).map((e) => e.id);
     if (getRates.includes(id)) {
       return ratingGoldStarIconSrc;
     } else {
