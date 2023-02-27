7bf604342f6ae12af9adcc97893d77b69ffe7f26
Yaroslav Nepiuk <slavanepuk@gmail.com>
2023-02-27 22:44:40 +0100

    feat: adding favicon & title

--- webpack.config.js
+++ webpack.config.js
@@ -41,6 +41,7 @@ const baseConfig = {
     new HtmlWebpackPlugin({
       template: path.resolve(__dirname, './src/index.html'),
       filename: 'index.html',
+      favicon: './src/assets/images/favicon.png',
     }),
 
     new MiniCssExtractPlugin({
